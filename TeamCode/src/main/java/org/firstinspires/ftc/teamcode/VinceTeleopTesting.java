c7b02653d1be3c0c431ebaed3143ab355a91fdec
vcher <vchernesky@gmail.com>
2021-01-24 19:26:24 -0500

    Wobble goal positions updated for using a NeverRest60

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VinceTeleopTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VinceTeleopTesting.java
@@ -117,8 +117,8 @@ public class VinceTeleopTesting extends OpMode
     public boolean              intakeRev = false;      // Track whether the intake is running in reverse
     public static double        STANDBY_SERVO = 0.77;   // Position for the servo to be in when not firing
     public static double        FIRE_SERVO = 1;         // Position for the servo when firing a ring
-    public static int           WOBBLE_GRAB = -840;     // Position for grabbing the wobble goal off the field
-    public static int           WOBBLE_OVER_WALL = -420; // Position for raising the wobble goal over the wall
+    public static int           WOBBLE_GRAB = -900;     // Position for grabbing the wobble goal off the field
+    public static int           WOBBLE_OVER_WALL = -550; // Position for raising the wobble goal over the wall
     public static int           WOBBLE_CARRY = -630;     // POsition for carrying the wobble goal to the wall
     public double lastwheelSpeeds[] = new double[4];     // Tracks the last power sent to the wheels to assist in ramping power
     public static double        SPEED_INCREMENT = 0.09;  // Increment that wheel speed will be increased/decreased
@@ -288,15 +288,15 @@ public class VinceTeleopTesting extends OpMode
             if(gamepad1.dpad_left){
                 wobbleMotor.setTargetPosition(WOBBLE_GRAB);
                 wobbleMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                wobbleMotor.setPower(1);
+                wobbleMotor.setPower(.4);
             } else if (gamepad1.dpad_right){
                 wobbleMotor.setTargetPosition(WOBBLE_CARRY);
                 wobbleMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                wobbleMotor.setPower(1);
+                wobbleMotor.setPower(.4);
             } else if (gamepad1.left_bumper) {
                 wobbleMotor.setTargetPosition(WOBBLE_OVER_WALL);
                 wobbleMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                wobbleMotor.setPower(1);
+                wobbleMotor.setPower(.4);
             }
         }
         // Insert operator driving actions here
