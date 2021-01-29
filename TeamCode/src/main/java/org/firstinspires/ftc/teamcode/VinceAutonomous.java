40bb4428ec9fc127abcb5d1627259d336db2f80a
LAMIA\Vince <vchernesky@gmail.com>
2021-01-28 23:37:40 -0500

    Crazy stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VinceAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VinceAutonomous.java
@@ -43,7 +43,7 @@ import java.util.List;
 import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
 
 @Autonomous(name="Vince Linear Autonomous", group="Linear Opmode")
-@Disabled
+//@Disabled
 public class VinceAutonomous extends LinearOpMode {
     // Declare OpMode members.
     public ElapsedTime runtime = new ElapsedTime();
@@ -190,6 +190,7 @@ public class VinceAutonomous extends LinearOpMode {
         telemetry.update();
         // Wait for the game to start (driver presses PLAY)
         //waitForStart();
+        tfod.activate();
         runtime.reset();
         while (!isStarted()) {
             // Put things to do prior to start in here
@@ -209,37 +210,38 @@ public class VinceAutonomous extends LinearOpMode {
 
             }
         }
-        tfod.shutdown(); //deinitialize object detection
 
-        initVuforiaNavigation(); //initialize and activate navigation
-        targetsUltimateGoal.activate();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            tfod.shutdown(); //deinitialize object detection
+
+            initNavigation(); //initialize and activate navigation
+            targetsUltimateGoal.activate();
 
             switch (wobbleBox){
                 case "1stBox":{ // Drop Zone A
                     // Drive forward 8 ft, turn right 45 degrees, and drop the goal
-                    driveByGyroEncoder(0,8*12);
+                    driveByGyroEncoder(.5,0,8*12);
                     // Turn right to 45 degrees by gyro
-                    gyroSpin(45);
+                    gyroSpin(-45);
                     // spit out the wobble goal
-                    intakeMotor.setPower(-.1);
+                    intakeMotor.setPower(.1);
                     sleep(2000);
                     intakeMotor.setPower(0);
                     // Turn back to zero degrees by gyro
                     gyroSpin(0);
                     // Strafe Right for a bit
-                    moveBot(0,getError(0),.5,.5);
-                    sleep(2000);
+                    //moveBot(0,getError(0),.5,.5);
+                    //sleep(2000);
                     stopBot();
                     // Drive forward until we see the goal
-                    while (!findRobotPosition()){
-                        moveBot(1,0,0,.5);
+                    while (!findRobotPosition() && !isStopRequested()){
+                        moveBot(.5,0,0,.5);
                     }
                     stopBot();
                     // Align to shoot
-                    while (!alignRobotToShoot()){
+                    while (!alignRobotToShoot() && !isStopRequested()){
                         moveBot(-.5,0,0,.5);
                     }
                     stopBot();
@@ -247,7 +249,7 @@ public class VinceAutonomous extends LinearOpMode {
                     shoot3Rings();
                     // Get over the white line by driving forward until we're on the positive side of the ield
 
-                    while (robotX < 0){
+                    while (robotX < 0 && !isStopRequested()){
                         moveBot(.1,0,0,.5);
                     }
                     stopBot();
@@ -313,14 +315,16 @@ public class VinceAutonomous extends LinearOpMode {
 
     }
 // This function drives on a gyro angle (deg) for a defined distance (in)
-    private void driveByGyroEncoder(double gyroAngle, double distance){
+    private void driveByGyroEncoder(double speed,double gyroAngle, double distance){
         double ticks = 0;
         int ppr = 134;  // Pulses per revolution
         double distPerRev = 3.14*4;
-        ticks = rightFrontDrive.getCurrentPosition() + (distance/distPerRev) * ppr;
+        ticks = leftFrontDrive.getCurrentPosition() + (distance/distPerRev) * ppr;
 
-        while (leftFrontDrive.getCurrentPosition() < ticks) {
-            moveBot(.5, getError(0), 0, .5);
+        while (leftFrontDrive.getCurrentPosition() < ticks && !isStopRequested()) {
+            moveBot(speed, .1 * getError(0), 0, .5);
+            telemetry.addData("Ticks", leftFrontDrive.getCurrentPosition());
+            telemetry.update();
         }
 
     }
@@ -361,7 +365,7 @@ public class VinceAutonomous extends LinearOpMode {
                 "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId); //comment to not use camera monitor
 
-        tfodParameters.minResultConfidence = 0.8f;
+        tfodParameters.minResultConfidence = 0.6f;
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
         tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
     }
@@ -668,4 +672,130 @@ public class VinceAutonomous extends LinearOpMode {
         }
         return targetVisible;
     }
+
+    private void initNavigation() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         * We can pass Vuforia the handle to a camera preview resource (on the RC phone);
+         * If no camera monitor is desired, use the parameter-less constructor instead (commented out below).
+         */
+        //Vuforia.deinit(); //comment if navigation is used first
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        //VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        //parameters.cameraDirection   = CAMERA_CHOICE;
+        parameters.useExtendedTracking = false;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+
+        // Make sure extended tracking is disabled for this example.
+        //parameters.useExtendedTracking = false;
+
+        //  Instantiate the Vuforia engine
+        //vuforia = new ClosableVuforiaLocalizer(parameters);
+
+        // Load the data sets for the trackable objects. These particular data
+        // sets are stored in the 'assets' part of our application.
+        System.out.println(vuforia.toString());
+        targetsUltimateGoal = this.vuforia.loadTrackablesFromAsset("UltimateGoal");
+        VuforiaTrackable blueTowerGoalTarget = targetsUltimateGoal.get(0);
+        blueTowerGoalTarget.setName("Blue Tower Goal Target");
+        VuforiaTrackable redTowerGoalTarget = targetsUltimateGoal.get(1);
+        redTowerGoalTarget.setName("Red Tower Goal Target");
+        VuforiaTrackable redAllianceTarget = targetsUltimateGoal.get(2);
+        redAllianceTarget.setName("Red Alliance Target");
+        VuforiaTrackable blueAllianceTarget = targetsUltimateGoal.get(3);
+        blueAllianceTarget.setName("Blue Alliance Target");
+        VuforiaTrackable frontWallTarget = targetsUltimateGoal.get(4);
+        frontWallTarget.setName("Front Wall Target");
+
+        // For convenience, gather together all the trackable objects in one easily-iterable collection */
+        allTrackables = new ArrayList<VuforiaTrackable>();
+        allTrackables.addAll(targetsUltimateGoal);
+
+        /**
+         * In order for localization to work, we need to tell the system where each target is on the field, and
+         * where the phone resides on the robot.  These specifications are in the form of <em>transformation matrices.</em>
+         * Transformation matrices are a central, important concept in the math here involved in localization.
+         * See <a href="https://en.wikipedia.org/wiki/Transformation_matrix">Transformation Matrix</a>
+         * for detailed information. Commonly, you'll encounter transformation matrices as instances
+         * of the {@link OpenGLMatrix} class.
+         *
+         * If you are standing in the Red Alliance Station looking towards the center of the field,
+         *     - The X axis runs from your left to the right. (positive from the center to the right)
+         *     - The Y axis runs from the Red Alliance Station towards the other side of the field
+         *       where the Blue Alliance Station is. (Positive is from the center, towards the BlueAlliance station)
+         *     - The Z axis runs from the floor, upwards towards the ceiling.  (Positive is above the floor)
+         *
+         * Before being transformed, each target image is conceptually located at the origin of the field's
+         *  coordinate system (the center of the field), facing up.
+         */
+
+        //Set the position of the perimeter targets with relation to origin (center of field)
+        redAllianceTarget.setLocation(OpenGLMatrix
+                .translation(0, -halfField, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 180)));
+
+        blueAllianceTarget.setLocation(OpenGLMatrix
+                .translation(0, halfField, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 0)));
+        frontWallTarget.setLocation(OpenGLMatrix
+                .translation(-halfField, 0, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 90)));
+
+        // The tower goal targets are located a quarter field length from the ends of the back perimeter wall.
+        blueTowerGoalTarget.setLocation(OpenGLMatrix
+                .translation(halfField, quadField, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, -90)));
+        redTowerGoalTarget.setLocation(OpenGLMatrix
+                .translation(halfField, -quadField, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, -90)));
+
+        //
+        // Create a transformation matrix describing where the phone is on the robot.
+        //
+        // NOTE !!!!  It's very important that you turn OFF your phone's Auto-Screen-Rotation option.
+        // Lock it into Portrait for these numbers to work.
+        //
+        // Info:  The coordinate frame for the robot looks the same as the field.
+        // The robot's "forward" direction is facing out along X axis, with the LEFT side facing out along the Y axis.
+        // Z is UP on the robot.  This equates to a bearing angle of Zero degrees.
+        //
+        // The phone starts out lying flat, with the screen facing Up and with the physical top of the phone
+        // pointing to the LEFT side of the Robot.
+        // The two examples below assume that the camera is facing forward out the front of the robot.
+
+        // We need to rotate the camera around it's long axis to bring the correct camera forward.
+        if (CAMERA_CHOICE == BACK) {
+            phoneYRotate = -90;
+        } else {
+            phoneYRotate = 90;
+        }
+
+        // Rotate the phone vertical about the X axis if it's in portrait mode
+        if (PHONE_IS_PORTRAIT) {
+            phoneXRotate = 90;
+        }
+
+        // Next, translate the camera lens to where it is on the robot.
+        // In this example, it is centered (left to right), but forward of the middle of the robot, and above ground level.
+        final float CAMERA_FORWARD_DISPLACEMENT = 4.0f * mmPerInch;   // eg: Camera is 4 Inches in front of robot center
+        final float CAMERA_VERTICAL_DISPLACEMENT = 8.0f * mmPerInch;   // eg: Camera is 8 Inches above ground
+        final float CAMERA_LEFT_DISPLACEMENT = 0;     // eg: Camera is ON the robot's center line
+
+        OpenGLMatrix robotFromCamera = OpenGLMatrix
+                .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, YZX, DEGREES, phoneYRotate, phoneZRotate, phoneXRotate));
+
+        /**  Let all the trackable listeners know where the phone is.  */
+        for (VuforiaTrackable trackable : allTrackables) {
+            ((VuforiaTrackableDefaultListener) trackable.getListener()).setPhoneInformation(robotFromCamera, parameters.cameraDirection);
+        }
+
+    }
+
 }
